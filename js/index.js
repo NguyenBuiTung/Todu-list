f795d67bb32f88e66d79f91c792c56dae0908a5f
nguyenbuitung <nguyenbuitung4@gmail.com>
2022-09-10 18:26:01 +0700

    render

--- js/index.js
+++ js/index.js
@@ -1,24 +1,45 @@
+
+let arr = []
 document.querySelector('#addItem').onclick = function () {
-    // console.log('tung')
-    let arr = []
     let valueinput = document.querySelector('#newTask').value
     arr.push(valueinput)
-    console.log(arr)
-    // let arrInput=[...arr]
-    // console.log(arrInput)
-    // console.log('first', valueinput)
-    // console.log('tung',valueinput)
+    // console.log(arr)
     let html = ''
-    for (let item of arr) {
+    for (let item in arr) {
         // console.log(item)
         let html1 = `
         <li >
-        <span>${valueinput}</span>
+        <span>${arr[item]}</span>
         <i class="fa fa-trash" aria-hidden="true"></i>
-        <i class="fa fa-check-circle" aria-hidden="true"></i>
+        <i class="fa fa-check-circle" aria-hidden="true" onclick="checkWork('${item}')"></i>
         </li>
         `
+        // console.log(item)
         html += html1
     }
     document.querySelector('#todo').innerHTML = html
 }
+// let arrInput=[...arr]
+window.checkWork = (id) => {
+    // let valueinput = document.querySelector('#newTask').value
+    // arr.push(valueinput)
+    // console.log(arrInput)
+    let html1 = ''
+    for (let item in arr) {
+        // console.log(item)
+        if (item === id) {
+            // console.log(arrInput[item])
+            let html2 = `
+        <li >
+        <span>${arr[item]}</span>
+        <i class="fa fa-trash" aria-hidden="true"></i>
+        <i class="fa fa-check-circle" aria-hidden="true"></i>
+        </li>
+        `
+            // console.log(item)
+            html1 += html2
+        }
+    }
+    document.querySelector('#completed').innerHTML = html1
+}
+
